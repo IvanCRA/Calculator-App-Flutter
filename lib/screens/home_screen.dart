0be1c1643fbe64aa25c4a89f0bb2302f16edc6b9
IvanCRA <101933328+IvanCRA@users.noreply.github.com>
2024-08-09 22:58:49 +0300

    add button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:calculator_app/constant/colors.dart';
+import 'package:calculator_app/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/textfield.dart';
@@ -5,6 +7,12 @@ import '../widgets/textfield.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.sizeOf(context).height;
+    final padding = EdgeInsets.symmetric(horizontal: 25, vertical: 30);
+    final decoration = BoxDecoration(
+        color: AppColors.primaryColor,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30)));
+
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 0, 0, 0),
       appBar: AppBar(
@@ -15,9 +23,20 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          CustomTextField(),
+          const CustomTextField(),
+          const Spacer(),
           Container(
-            height: MediaQuery.sizeOf(context).height * 0.6,
+            height: screenHeight * 0.6,
+            width: double.infinity,
+            padding: padding,
+            decoration: decoration,
+            child: Column(
+              children: [
+                Row(
+                  children: [Button1()],
+                )
+              ],
+            ),
           )
         ],
       ),
